2b2d93ef86250790b7490c18385c96ca72ff5cc2
IAmLawrence <lawrencelanzaderas020497@gmail.com>
2021-12-12 16:19:44 +0800

    CHANGES

--- practice_module/__manifest__.py
+++ practice_module/__manifest__.py
@@ -22,6 +22,7 @@
     # any module necessary for this one to work correctly
     'depends': [
         'base',
+        'web_notify',
         'web_widget_model_viewer',
     ],
 
--- practice_module/models/my_library.py
+++ practice_module/models/my_library.py
@@ -13,3 +13,27 @@ class MyBooks(models.Model):
         ('damage', 'Damage'),
     ], string="Status")
     desc = fields.Text(string="Description")
+
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('confirm', 'Confirm'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')],
+        string='Status', default='draft')
+
+    user_id = fields.Many2one('res.users', string="User ID")
+
+    def btn_draft(self):
+        self.state = 'draft'
+
+    def btn_confirm(self):
+        self.state = 'confirm'
+        self.user_id.notify_danger("Okay")
+
+    def btn_done(self):
+        self.state = 'done'
+
+    def btn_cancel(self):
+        self.state = 'cancel'
+
+
